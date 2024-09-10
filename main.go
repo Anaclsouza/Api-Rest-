6a2cb60cfe2e5baa5a1998e3eaec2bb60021f599
Ana Clara <claranogueiramg@gmail.com>
2024-09-10 20:58:37 -0300

    Criando uma HomePage de Floricultura usando Struct com Html

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"text/template"
+)
+
+type Planos struct {
+	PlanoMensal string
+	Preço float64
+	Descritivo string
+
+}
+
+// Acessando aos templates(paginas) que serão usados
+var temp = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
 
-	fmt.Println(" meu primeiro commit ")
+	http.HandleFunc("/", Index) //Essa Func afirma que ao receber uma requisição ( representado por : "/")
+	// deve apontar para o Template Index
+
+	//Criando o servidor no qual hospedaremos nossos templates
+	http.ListenAndServe(":8000", nil)
+
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	planos :=[]Planos{
+		{"Plano Simples", 300, " Dois Buques de rosas personalizados ana"},
+		{"Plano Premium", 650, " Quatro Buques de rosas personalizados"},
+	}
+	temp.ExecuteTemplate(w,"Index", planos)
 }
